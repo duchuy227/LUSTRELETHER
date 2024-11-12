55b82827825c3166140c815651b5d290461dd18d
Duc Huy <103496061+duchuy2278@users.noreply.github.com>
2024-11-12 09:55:35 +0700

    h

--- .history/Views/Layout/homepage_header_20241111142533.php
+++
@@ -1,6 +0,0 @@
-<div class="hero-section">
-    <img height="400" src="././views/Img/u35.svg" width="800"/>
-    <h1>
-        The influencer community is a place to connect inspirational individuals and spread positive values to people
-    </h1>
-</div>
